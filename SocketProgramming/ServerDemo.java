545c6dab3beb569188d5c13bc5d1bd03b19e7c09
Praveen Kumar Yadav <praveenyadavskit@gmail.com>
2021-11-12 01:36:15 +0530

    Code Updated

--- SocketProgramming/ServerDemo.java
+++ SocketProgramming/ServerDemo.java
@@ -3,11 +3,14 @@ import java.io.*;
 class ServerDemo{
     public static void main(String args[]) throws IOException{
         //Step 1: Create a ServerSocket on port 777
+        System.out.println("Server is started");
         ServerSocket ss=new ServerSocket(777);
 
         //Step 2: Created Server will wait till the client application is 
         //accepted the connection
+        System.out.println("Server is waiting for the client request");
         Socket s=ss.accept();
+        System.out.println("Server is connected to the client");
 
         //Step 3: Attach an output stream to the server
         OutputStream obj=s.getOutputStream();
